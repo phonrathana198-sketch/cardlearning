1eece2ea8d459fa76072f7eea867ec22e663d386
cydersnake.eyes ng <phonrathana198@gmail.com>
2026-02-18 02:37:05 +0700

    Add MainActivity with ViewPager and TabLayout

---
+++ Add Dependencies/MainActivity.kt
@@ -0,0 +1,22 @@
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val viewPager = findViewById<ViewPager2>(R.id.viewPager)
+        val tabLayout = findViewById<TabLayout>(R.id.tabLayout)
+
+        val adapter = ViewPagerAdapter(this)
+        viewPager.adapter = adapter
+
+        TabLayoutMediator(tabLayout, viewPager) { tab, position ->
+            tab.text = when(position) {
+                0 -> "Basic"
+                1 -> "Intermediate"
+                2 -> "Advance"
+                else -> "My Apps"
+            }
+        }.attach()
+    }
+}
